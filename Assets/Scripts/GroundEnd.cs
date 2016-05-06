4b082b733fdc247d3ad732dbf500fd648b9492a7
Eduardo_Vazquez <eduardo.vazquezn@gmail.com>
2016-05-06 23:00:09 +0200

    Cambios en el scirpt de la nave y parallax en proceso

---
+++ Assets/Scripts/GroundEnd.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundEnd : MonoBehaviour {
+
+    private ParallaxManager manager;
+           
+    // Use this for initialization
+    void Start () {
+        manager = GetComponentInParent<ParallaxManager>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        manager.GroundDisabled(col.GetComponent<Ground>());
+
+
+    }
+}
