46c9dd0d43533b2d76cd147be16dd335967e1d2d
Daventor <davelop@gmail.com>
2016-05-07 21:25:14 +0200

    no message

---
+++ Assets/Scripts/GUIActions.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GUIActions : MonoBehaviour {
+
+     public void GoToCredits(){
+          SceneManager.LoadScene ("Credits");
+     }
+
+     public void GoToNormalGame(){
+          SceneManager.LoadScene ("NormalGame");
+     }
+
+     public void GoToVRGame(){
+          SceneManager.LoadScene ("VRGame");
+     }
+
+     public void GoToMainMenu(){
+          SceneManager.LoadScene ("MainMenu");
+     }
+
+     public void RestartGame(){
+          Scene scn = SceneManager.GetActiveScene ();
+          SceneManager.LoadScene (scn.name);
+     }
+
+     public void OpenExitPanel(){
+          
+     }
+
+     public void CloseExitPanel(){
+
+     }
+
+     public void ExitGame(){
+          Application.Quit ();
+          UnityEditor.EditorApplication.isPlaying = false;
+     }
+
+     public void OpenMenuPanel(){
+          
+     }
+
+     public void CloseMenuPanel(){
+          
+     }
+}
